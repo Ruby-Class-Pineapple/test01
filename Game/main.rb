27b7a4c9fc343757cc2d7be15a381f61f0eeee96
s235004 <s2350004@matsu.shimane-u.ac.jp>
2024-05-24 16:36:20 +0900

    タイトル画面の処理をtittle.rbに以降

--- Game/main.rb
+++ Game/main.rb
@@ -1,4 +1,5 @@
 require "dxruby"
+require "scene_switcher"
 
 # 画面サイズ指定(1920×1080)
 Window.width  = 1920
@@ -10,17 +11,24 @@ Window.bgcolor = C_WHITE
 # ゲームのタイトルを設定
 Window.caption = "おじタイピング"
 
-# ゲームの画像を読み込み
-image1 = Image.load("Image_home/png/dec_hige.png")
-image2 = Image.load("Image_home/png/dec_logo.png")
-image3 = Image.load("Image_home/png/button_start.png")
+# フォントの設定
+font = Font.new(24)
 
 Window.loop do
-    Window.draw(960, 540, image1)
-    Window.draw(960, 700, image2)
-    Window.draw(960, 400, image3)
+    Window.draw_font(960, 540, "1:tittle,2:menu,3:game,4:result", font,color:[0,0,0] )
+    if Input.key_push?(K_1)
+        switch_to "tittle.rb"
+    end
+    if Input.key_push?(K_2)
+        switch_to "menu.rb"
+    end
+    if Input.key_push?(K_3)
+        switch_to "game.rb"
+    end
+    if Input.key_push?(K_4)
+        switch_to "result.rb"
+    end
 end
-
 
 # 参考サイト
 # https://qiita.com/noanoa07/items/bced6519d9b53685b651
