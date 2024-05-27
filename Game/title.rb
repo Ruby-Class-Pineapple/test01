dbc965406f7a4a440432487231a3deb1e84d9be2
s235004 <s2350004@matsu.shimane-u.ac.jp>
2024-05-27 19:50:37 +0900

    シーン遷移機能を実装

---
+++ Game/title.rb
@@ -0,0 +1,22 @@
+module GameTitle
+
+    # ゲームの画像を読み込み
+    @image1 = Image.load("Image_home/png/dec_hige.png")
+    @image2 = Image.load("Image_home/png/dec_logo.png")
+    @image3 = Image.load("Image_home/png/button_start.png")
+
+    module_function
+    def exec
+        
+        # 画像を描写
+        Window.draw(960, 540, @image1)
+        Window.draw(960, 700, @image2)
+        Window.draw(960, 400, @image3)
+
+        # スペースキーでメニュー画面に遷移
+        if Input.key_push?(K_SPACE)
+            $scene = 2
+        end
+
+    end
+end
