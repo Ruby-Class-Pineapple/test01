e877e8d590b1c29f3bdfaa3804d98e077a929d33
s235004 <s2350004@matsu.shimane-u.ac.jp>
2024-05-31 15:23:16 +0900

    ゲームシーンのUI・終了ボタン・タイマーの実装

--- Game/game.rb
+++ Game/game.rb
@@ -1,13 +1,105 @@
 module GameMain
 
-  # 書式設定
-  @font = Font.new(32)
+  @time = 60 # タイマー用
+  
+  @key_maker = Image.load("Image_game/key_marker.png") # テスト用
+  @dec_logo = Image.load("Image_home/dec_logo.png")  # 終了ボタン
+  @back_blue = Image.new(1920, 500, [151, 170, 212]) # 背景
+  @dec_bar = Image.load("Image_game/dec_bar.png") # バー
+  @button_end = Image.load("Image_menu/button_end.png") # 終了ボタン
+
+  # タイムバー
+  @time_font = Font.new(25) # 書式設定
+  @time_bars = (1..10).map { |i| Image.load("Image_game/time_bar_#{i}.png") }
+  @timer_started = false # カウントダウン開始フラグ
+
+  # キーボード
+  @key_a_1 = Image.load("Image_game/key_a_1.png") # a
+  @key_a_2 = Image.load("Image_game/key_a_2.png") # a
+  @key_a_3 = Image.load("Image_game/key_a_3.png") # a
 
   module_function
+
+  # タイムバー
+  def time_bar
+
+    # カウントダウン開始
+    unless @timer_started
+      @start_time = Time.now
+      @timer_started = true
+    end
+
+    Window.draw_font(30, 105, "< 田中文雄", @time_font,color:[255,255,255])
+    Window.draw_font(1200, 105, "残り時間", @time_font,color:[255,255,255])
+
+    # 経過時間を計算
+    elapsed_time = Time.now - @start_time
+
+    # 現在の表示すべき画像のインデックスを計算
+    current_index = [(elapsed_time / (@time / 10)).floor, 9].min
+
+    # 画像の描画
+    Window.draw(1330, 105, @time_bars[current_index])
+
+    # 残り時間表示
+    Window.draw_font(1830, 105, "#{(@time - elapsed_time).floor} 秒", @time_font,color:[255,255,255])
+
+    # 経過時間が@timeを超えたらループを終了
+    if elapsed_time >= @time
+      $scene = 4
+    end
+
+  end
+
+  # ボタン実装
+  def button(button_x,button_y)
+
+    # 終了ボタンの座標とサイズ
+    button_width = @button_end.width
+    button_height = @button_end.height
+
+    # マウスの位置を取得
+    mouse_x = Input.mouse_pos_x
+    mouse_y = Input.mouse_pos_y
+
+    # ボタンの上にカーソルがあるとき
+    if mouse_x >= button_x && mouse_x <= button_x + button_width &&
+      mouse_y >= button_y && mouse_y <= button_y + button_height
+
+      Input.set_cursor(IDC_HAND) # マウスカーソルを手の形状に変更
+
+      # マウスクリックの検出
+      if Input.mouse_push?(M_LBUTTON)
+        $scene = 1
+      end
+
+    else
+      Input.set_cursor(IDC_ARROW) # デフォルトのマウスカーソルを使用
+    end
+
+  end
+
   def exec
 
-    # 文字の出力
-    Window.draw_font(400, 500, "ゲームシーンです。", @font,color:[0,0,0])
+    Window.draw(0, 90, @back_blue)
+
+    # 終了ボタン
+    Window.draw(1725, 3, @button_end)
+    button(1725,3)
+
+    Window.draw_scale(-180, -5, @dec_logo,0.4,0.4)
+    Window.draw(-12,65,@dec_bar)
+
+    time_bar() # タイムバーの実行
+
+    Window.draw(22, 610, @key_maker)
+
+    if Input.key_down?(K_A)
+      Window.draw(0, 500, @key_a_2)
+    else
+      Window.draw(0, 500, @key_a_1)
+    end
+   
 
     # スペースキーで結果画面に遷移
     if Input.key_push?(K_SPACE)
@@ -15,4 +107,5 @@ module GameMain
     end
 
   end
+
 end
