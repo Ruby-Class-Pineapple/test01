dabe945420e56edbc5a8658827bac84268e1f88a
a233005-ita <a233005@matsu.shimane-u.ac.jp>
2024-05-31 16:41:25 +0900

    タイトルの変更

--- Game/title.rb
+++ Game/title.rb
@@ -5,13 +5,19 @@ module GameTitle
     @image2 = Image.load("Image_home/dec_logo.png")
     @image3 = Image.load("Image_home/button_start.png")
 
+    # 書式設定
+    @font = Font.new(32)
+
     module_function
     def exec
         
         # 画像を描写
-        Window.draw(960, 540, @image1)
-        Window.draw(960, 700, @image2)
-        Window.draw(960, 400, @image3)
+        Window.draw(850, 440, @image1)
+        Window.draw(570, 330, @image2)
+        Window.draw(690, 590, @image3)
+
+        # 文字の出力
+        Window.draw_font(755, 900, "＠Powered by Pineapple", @font,color:[0,0,0])
 
         # スペースキーでメニュー画面に遷移
         if Input.key_push?(K_SPACE)
